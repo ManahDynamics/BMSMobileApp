aadbaadb2025ae436576a5594a38cfd5b3ebda3b
Anudeep Mummaneni <anudeep@manah.com>
2026-06-19 14:33:05 +0530

    Chnage sin the login  screen

--- lib/modules/auth/screens/login_screen.dart
+++ lib/modules/auth/screens/login_screen.dart
@@ -71,7 +71,7 @@ class _LoginScreenState extends State<LoginScreen> {
     if (isLoggedIn && mounted) {
       Future.delayed(Duration.zero, () {
         if (mounted) {
-          Navigator.pushReplacementNamed(context, '/dashboard');
+          Navigator.pushReplacementNamed(context, '/connect');
         }
       });
     }
@@ -299,7 +299,7 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
         );
 
-        Navigator.pushReplacementNamed(context, '/dashboard');
+        Navigator.pushReplacementNamed(context, '/connect');
       } else {
         final serverMessage = rawJson['message']?.toString() ??
             rawJson['error']?.toString() ??
