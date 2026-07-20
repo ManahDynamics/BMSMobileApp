2a2200162c2a65bb7cc97c9b0fa1783c6316b0e0
Anudeep Mummaneni <anudeep@manah.com>
2026-07-20 16:55:13 +0530

    Chnaged is text size

--- lib/modules/dashboard/screens/dashboard_screen.dart
+++ lib/modules/dashboard/screens/dashboard_screen.dart
@@ -1379,11 +1379,11 @@ Color _voltageColor(double v) {
               children: [
                 Text(
                   '${tr('dashboard.min_volt')} $minVoltage',
-                  style: const TextStyle(fontSize: 11, color: Color(0xFF5E5E5E), fontWeight: FontWeight.w500, letterSpacing: 0),
+                  style: const TextStyle(fontSize: 11, color: Color(0xFF5E5E5E), fontWeight: FontWeight.w700, letterSpacing: 0),
                 ),
                 Text(
                   '${tr('dashboard.max_volt')} $maxVoltage',
-                  style: const TextStyle(fontSize: 11, color: Color(0xFF5E5E5E), fontWeight: FontWeight.w500, letterSpacing: 0),
+                  style: const TextStyle(fontSize: 11, color: Color(0xFF5E5E5E), fontWeight: FontWeight.w700, letterSpacing: 0),
                 ),
               ],
             ),
